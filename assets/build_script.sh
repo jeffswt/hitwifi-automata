ccdfe4a1ea52d1d976d5606c18a3c32624d46e05
Geoffrey Tang <jeffswt@users.noreply.github.com>
2019-08-09 18:43:39 +0800

    Add icon converting script.

--- assets/build_script.sh
+++ assets/build_script.sh
@@ -2,4 +2,5 @@ pipenv install
 pipenv shell
 pip install -r requirements.txt
 pip install pyinstaller
-pyinstaller --onefile --windowed --icon ../assets/icon.png hitwa.pyw
+ffmpeg -i ../assets/icon.png ../assets/icon.ico
+pyinstaller --onefile --windowed --icon ../assets/icon.ico hitwa.pyw
